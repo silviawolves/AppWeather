9379887dc2c9fc01459902949afac4e3650e4726
silviawolves <silviacanepa.w@gmail.com>
2022-11-15 15:46:51 +0100

    correct forecast with 5 days

--- src/components/Forecast2/Forecast2.tsx
+++ src/components/Forecast2/Forecast2.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useDebugValue} from 'react';
 import dayjs from 'dayjs';
 
 import {useState, useEffect} from 'react';
@@ -20,23 +20,6 @@ const Forecast2 = ({weather}: Props) => {
             .then((response) => response.json())
             .then((data) => {
                 setIsLoaded(true);
-                // const forecast: forecast = {
-                //     list: [
-                //         {
-                //             dt: data.list[0].dt,
-                //             main: {
-                //                 temp_max: data.list[0].main.temp_max,
-                //                 temp_min: data.list[0].main.temp_min,
-                //             },
-                //             weather: [
-                //                 {
-                //                     main: data.list[0].weather[0].main,
-                //                     icon: data.list[0].weather[0].icon,
-                //                 },
-                //             ],
-                //         },
-                //     ],
-                // };
                 setForecast(data.list);
             })
             .catch((error) => {
@@ -44,59 +27,47 @@ const Forecast2 = ({weather}: Props) => {
             });
     }, [weather]);
 
-    const day = (forecast: any) => {
-        Object.values(forecast).map((data: any) => {
-            const days = dayjs(data.dt * 1000).format('dddd');
-            const dayIcon = data.weather[0].icon;
-            const dayMaxTemp = data.main.temp_max;
-            const dayMinTemp = data.main.temp_min;
-            console.log(
-                Math.round(dayMaxTemp),
-                Math.round(dayMinTemp),
-                dayIcon,
-            );
-            console.log('weather ' + days);
-            // .filter(
-            //     (value, i, self) =>
-            //         i === self.findIndex((day) => day.dt === value.dt),
-            // );
-        });
+    const formatDays = (forecast: any) => {
+        return Object.values(forecast)
+            .map((data: any) => {
+                return {
+                    days: dayjs(data.dt * 1000).format('dddd'),
+                    dayIcon: data.weather[0].icon,
+                    dayMaxTemp: data.main.temp_max,
+                    dayMinTemp: data.main.temp_min,
+                };
+            })
+            .filter(
+                (value: any, i: number, self: any) =>
+                    i === self.findIndex((day: any) => day.days === value.days),
+            )
+            .slice(0, 6);
     };
-
-    // const filteredDays = (day) => {
-    //     return day.filter(
-    //         (value, i, self) =>
-    //             i === self.findIndex((day) => day.dt === value.dt),
-    //     );
-    // };
-    // console.log(filteredDays());
 
     if (!isLoaded) {
         return <Text>Loading...</Text>;
     } else {
         return (
             <View style={{paddingLeft: 20, paddingRight: 20, paddingTop: 70}}>
-                {Object.values(forecast)
+                {formatDays(forecast)
                     .slice(0, 5)
                     .map((data: any, i) => {
                         return (
                             <View style={styles.forecast} key={i}>
-                                <Text style={styles.daily}>
-                                    {dayjs(data.dt * 1000).format('dddd')}
-                                </Text>
+                                <Text style={styles.daily}>{data.days}</Text>
 
                                 <View style={styles.weather}>
                                     <Image
                                         source={{
-                                            uri: `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`,
+                                            uri: `https://openweathermap.org/img/wn/${data.dayIcon}.png`,
                                         }}
                                         style={styles.img}
                                     />
                                 </View>
 
                                 <Text style={styles.temperature}>
-                                    ↓ {Math.round(data.main.temp_min)}° |{' '}
-                                    {Math.round(data.main.temp_max)}° ↑
+                                    ↓ {Math.round(data.dayMaxTemp)}° |{' '}
+                                    {Math.round(data.dayMinTemp)}° ↑
                                 </Text>
                             </View>
                         );
